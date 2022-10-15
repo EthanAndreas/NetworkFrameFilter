cfe76c3091f231303931146a97d2e10a92e57931
Ethan Huret <ethan.huret@etu.unistra.fr>
2022-10-15 10:00:51 +0200

    vendor specific class version completed

--- include/4_bootp.h
+++ include/4_bootp.h
@@ -3,7 +3,8 @@
 
 #include "../include/include.h"
 
-void print_dhcp_option(uint8_t bp_vend[64], int i);
+void print_dhcp_option_addr(uint8_t bp_vend[64], int i);
+void print_dhcp_option_name(uint8_t bp_vend[64], int i);
 void bootp_vendor_specific(uint8_t bp_vend[64]);
 void bootp_analyzer(const u_char *packet);
 
--- src/4_bootp.c
+++ src/4_bootp.c
@@ -1,20 +1,40 @@
 #include "../include/4_bootp.h"
 
-void print_dhcp_option(uint8_t bp_vend[64], int i) {
+void print_dhcp_option_addr(uint8_t bp_vend[64], int i) {
 
     int j;
     for (j = 1; j <= bp_vend[i + 1]; j++) {
-        if (j != 1 && j != bp_vend[i + 1])
+        if (j != 1 && j != bp_vend[i + 1] + 1)
             printf(".");
         printf("%d", bp_vend[i + j + 1]);
     }
+    printf("\n");
+}
+
+void print_dhcp_option_name(uint8_t bp_vend[64], int i) {
+
+    int j;
+    for (j = 1; j <= bp_vend[i + 1]; j++) {
+        printf("%c", bp_vend[i + j + 1]);
+    }
+    printf("\n");
+}
+
+void print_dhcp_option_time(uint8_t bp_vend[64], int i) {
+
+    uint32_t time = 0;
+    time += bp_vend[i + 2] << 24;
+    time += bp_vend[i + 3] << 16;
+    time += bp_vend[i + 4] << 8;
+    time += bp_vend[i + 5];
+    printf("%d\n", time);
 }
 
 void bootp_vendor_specific(uint8_t bp_vend[64]) {
 
     if (bp_vend[0] == 0x63 && bp_vend[1] == 0x82 &&
         bp_vend[2] == 0x53 && bp_vend[3] == 0x63) {
-        printf("DHCP protocol, ");
+        printf("DHCP protocol\n");
     }
 
     int i, j;
@@ -25,14 +45,59 @@ void bootp_vendor_specific(uint8_t bp_vend[64]) {
         case TAG_SUBNET_MASK:
 
             printf("Subnet mask : ");
-            print_dhcp_option(bp_vend, i);
+            print_dhcp_option_addr(bp_vend, i);
             i += bp_vend[i + 1] + 1;
             break;
         case TAG_TIME_OFFSET:
-            printf("Time offset : %d, ", bp_vend[i + 1]);
+            printf("Time offset : ");
+            print_dhcp_option_time(bp_vend, i);
             i += 5;
             break;
-
+        case TAG_GATEWAY:
+            printf("Router : ");
+            print_dhcp_option_addr(bp_vend, i);
+            i += bp_vend[i + 1] + 1;
+            break;
+        case TAG_DOMAIN_SERVER:
+            printf("DNS : ");
+            print_dhcp_option_addr(bp_vend, i);
+            i += bp_vend[i + 1] + 1;
+            break;
+        case TAG_HOSTNAME:
+            printf("Hostname : ");
+            print_dhcp_option_name(bp_vend, i);
+            i += bp_vend[i + 1] + 1;
+            break;
+        case TAG_DOMAINNAME:
+            printf("Domain name : ");
+            print_dhcp_option_name(bp_vend, i);
+            i += bp_vend[i + 1] + 1;
+            break;
+        case TAG_BROAD_ADDR:
+            printf("Broadcast : ");
+            print_dhcp_option_addr(bp_vend, i);
+            i += bp_vend[i + 1] + 1;
+            break;
+        case TAG_NETBIOS_NS:
+            printf("Netbios name server : ");
+            print_dhcp_option_addr(bp_vend, i);
+            i += bp_vend[i + 1] + 1;
+            break;
+        case TAG_NETBIOS_SCOPE:
+            printf("Netbios scope : ");
+            print_dhcp_option_name(bp_vend, i);
+            i += bp_vend[i + 1] + 1;
+            break;
+        case TAG_REQUESTED_IP:
+            printf("Requested IP address : ");
+            print_dhcp_option_addr(bp_vend, i);
+            i += bp_vend[i + 1] + 1;
+            break;
+        case TAG_IP_LEASE:
+            printf("Lease time : ");
+            print_dhcp_option_time(bp_vend, i);
+            i += 5;
+            break;
         // DCHP options
         case TAG_DHCP_MESSAGE:
             printf("DHCP options : ");
@@ -68,6 +133,13 @@ void bootp_vendor_specific(uint8_t bp_vend[64]) {
             }
 
             i += bp_vend[i + 1] + 1;
+            break;
+        case TAG_SERVER_ID:
+            printf("DHCP server : ");
+            print_dhcp_option_addr(bp_vend, i);
+            i += bp_vend[i + 1] + 1;
+            break;
+        case TAG_PARM_REQUEST:
             break;
         default:
             break;
