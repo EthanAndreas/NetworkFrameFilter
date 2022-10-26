c250daacdc4f2a339d7abc7156e613e3c21e66cf
Ethan Huret <ethan.huret@etu.unistra.fr>
2022-10-26 14:05:11 +0200

    save

--- src/2_ip.c
+++ src/2_ip.c
@@ -6,24 +6,27 @@ struct ip *ip_analyzer(const u_char *packet, int verbose) {
 
     PRV1(printf(GRN "IP Header" NC "\n"), verbose);
 
-    PRV1(printf("IP version : %d", ip->ip_v), verbose);
+    PRV1(printf("IP version : %d\n", ip->ip_v), verbose);
 
     if (ip->ip_p == IPPROTO_TCP)
-        PRV1(printf(", Protocol : TCP"), verbose);
+        PRV1(printf("Protocol : TCP\n"), verbose);
 
     if (ip->ip_p == IPPROTO_UDP)
-        PRV1(printf(", Protocol : UDP"), verbose);
+        PRV1(printf("Protocol : UDP\n"), verbose);
 
-    PRV1(printf(", Address IP source : %s,", inet_ntoa(ip->ip_src)),
+    PRV1(printf("Address IP source : %s\n", inet_ntoa(ip->ip_src)),
          verbose);
 
-    PRV1(printf(", Address IP destination : %s\n",
+    PRV1(printf("Address IP destination : %s\n",
                 inet_ntoa(ip->ip_dst)),
          verbose);
 
-    PRV2(printf("\tIP Size: %d, Thread size : %d\n", ip->ip_hl,
-                ip->ip_len),
+    PRV2(printf("\tIP Size: %d\n"
+                "\tThread size : %d\n",
+                ip->ip_hl, ip->ip_len),
          verbose);
+
+    // type service
 
     return ip;
 }
