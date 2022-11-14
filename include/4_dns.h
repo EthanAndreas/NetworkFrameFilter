39cfc5a5ff93b6ebc955d540145801b8c8727409
Ethan Huret <ethan.huret@etu.unistra.fr>
2022-11-14 23:27:25 +0100

    begin dns

--- include/4_dns.h
+++ include/4_dns.h
@@ -3,6 +3,15 @@
 
 #include "../include/include.h"
 
+struct dns_hdr {
+    u_int16_t id;
+    u_int16_t flags;
+    u_int16_t qdcount;
+    u_int16_t ancount;
+    u_int16_t nscount;
+    u_int16_t arcount;
+};
+
 void dns_analyzer(const u_char *packet, int lenght, int verbose);
 
 #endif
--- src/4_dns.c
+++ src/4_dns.c
@@ -1,5 +1,34 @@
 #include "../include/4_dns.h"
 
 void dns_analyzer(const u_char *packet, int length, int verbose) {
-    printf("DNS\n");
+
+    PRV1(printf(GRN "DNS Protocol" NC "\n"), verbose);
+
+    struct dns_hdr *dns_header = (struct dns_hdr *)packet;
+
+    PRV2(printf("\tTransaction ID : 0x%0x\n", ntohs(dns_header->id)),
+         verbose);
+
+    PRV2(printf("\tFlags : 0x%0x", ntohs(dns_header->flags)),
+         verbose);
+    if (ntohs(dns_header->flags) & 0x8000) {
+        PRV2(printf(" (Response)\n"), verbose);
+    } else {
+        PRV2(printf(" (Query)\n"), verbose);
+    }
+
+    PRV3(printf("\t\tQuestions : %d\n"
+                "\t\tAnswer RRs : %d\n"
+                "\t\tAuthority RRs : %d\n",
+                ntohs(dns_header->qdcount),
+                ntohs(dns_header->ancount),
+                ntohs(dns_header->nscount)),
+         verbose);
+
+    // - function to parse the query (execute the number of qdcount
+    // times)
+    //- function to parse the answer (execute the number of
+    // ancount times)
+    // - function to parse the authority (execute the
+    // number of nscount times)
 }
